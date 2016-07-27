f1329408b97f1419588f96e86f4331c82143b019
erick navarro <erick.navarro150@gmail.com>
2016-07-27 13:04:50 -0500

    Servidor funcionando - falta diseño y estetica

--- internals/fortune.js
+++ internals/fortune.js
@@ -1,61 +1,69 @@
- //Cargando el driver de Mongodb que
- //me permitira conectarme a la base de datos
+// 1. Cargando el Driver de Mongodb que
+// me permitira conectarme a la base de datos
+var mongodb = require("mongodb");
+// 2. Cargo al cliente de driver
+var mongoClient = mongodb.MongoClient;
 
- var mongodb = require ("mongodb");
+module.exports = {
+    "getFortune" : function(cb){
+        // Conectando el cliente a la base de datos fortune
+        //var connectionString = "mongodb://127.0.0.1:27017/fortune";
+        //var connectionString = 
+        //"mongodb:///fortune";  
+        mongoClient.connect("mongodb://127.0.0.1:27017/fortune",
+        function(err, db){
+            if(err){
+                console.log("> ERROR al conectarse a" +
+                " la base de datos: "+ "mongodb://127.0.0.1:27017/fortune");
+                var fortunePaper = {
+                    "mesagge":
+                    "La honestidad es un regalo caro, no lo esperes de gente barata"
+                };
+                // Convirtiendo el fortunePaper de objeto
+                // a su version en string
+                var fortunePaperResponse = JSON.stringify(fortunePaper);
+                
+                // Ejecuto el callback (cb) pasandole
+                // como parametro el fortunepaper string
+                cb(fortunePaperResponse);
+            }else{
+                // Obtengo la coleccion con la que quiero trabajar
+                var papersCollection = 
+                db.collection("papers");
+                
+                // Consulto todos los documentos de mi coleccion
+                var objetoRestultado = 
+                papersCollection.find({});
 
- //Cargo al cliente de driver
-
- var mongoClient = mongodb.MongoClient;
-
- module.exports = {
-      "getFortune" : function(cb){
-          //conectando el cliente a la base de datos fortune
-          mongoClient.connect("mongodb://127.0.0.1:27017/fortune",
-          function (err, db) {
-              if(err){
-                  console.log("Error al conectarse a" +
-                  "la base de datos" +
-                  "mongodb://127.0.0.1:27017/fortune");
-                  var fortunePaper = {
-    "mensaje":
-     "La honestidad es un regalo caro, no lo esperes de gente barata"
- };
-           
-
-
-          // Convirtiendo el fortunePaper de objeto
-          // a su version en string
-
-        var fortunePaperResponse = JSON.stringify(fortunePaper);
-          // Ejecuto el callback (cb) pasandole
-          // como parametro el fortunepaper string
- 
-         //cb(fortunePaperResponse);
-       }else{
-           //obtengo la collection con la que quiero trabajar
-           var papersCollection = db.collection("papers");
-           //Consulto todos los documentos de mi coolleccion
-           var objetoResultado = papersCollection.find({});
-
-           //parseo el objetoResultado en un arreglo
-           objetoResultado.toArray(function (err, papers) {
-               var fortunePaperResponse = 
-               JSON.stringify(papers[0]);
-               //cerrar la conexion entre el cliente
-               //y la base de datos
-
-               db.close()
-               //Invoco al cb pasadole como paramtero
-               //la respuesta
-               cb(fortunePaperResponse); 
-           })
-
-       }
-          });  
+                // Parseo el objeto resultado en un arreglo
+                objetoRestultado.toArray(function(err, papers){
+                    // Obtengo un indide aleatorio
+                    // contemplando como min = 1
+                    // Y como max = la logitud de arreglo papers
+                    var randomIndex = 
+                    getRandomArbitrary(1, papers.length);
+                    console.log("> RandomIndex calculated: " + randomIndex);
+                    var fortunePaperResponse = 
+                    JSON.stringify(papers[randomIndex]);
+                    // Cerrar la conexion entre el cliente
+                    // y la base de datos
+                    db.close()
+                    // Invoco al cb pasandole como parametro
+                    // la respuesta
+                    console.log("> La fortuna es: " + fortunePaperResponse);
+                    cb(fortunePaperResponse);
+                });
+            }
+        });
     }
+};
 
-  };
-  // var fortunePaper = {
-  //   "mensaje":
-    // "La honestidad es un regalo caro, no lo esperes de gente barata"
-// };
+// Devuelve un número aleatorio entre min (ambos inclusive ) y máximo (exclusivo )
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+
+
--- static/js/site.js
+++ static/js/site.js
@@ -2,10 +2,11 @@
      // Realizando la solicitud get en AJAX
      $.get("/getfortune","", function(data, status){
          console.log("> " +  typeof(data));
+         console.log("> Mensaje: " + typeof(data.mesagge));
          console.log("> Estatus de respuesta: " + status);
          swal({
              title: "¡TU FORTUNA!",
-             text: data.mensaje,
+             text: data.mesagge,
               imageUrl: "img/gl.png"
           });
       },"json");
